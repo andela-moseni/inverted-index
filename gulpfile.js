db6ec2303812ba906d844a197bbbf524dbf4bd40
andela-moseni <mercy.oseni@andela.com>
2017-03-18 12:41:29 +0100

    create gulp config

---
+++ gulpfile.js
@@ -0,0 +1,29 @@
+// Gulp configurations to reload browser when files change
+const gulp = require('gulp');
+const browserSync = require('browser-sync').create();
+const browserify = require('gulp-browserify');
+const rename = require('gulp-rename');
+
+gulp.task('browserSync', function() {
+    browserSync.init({
+       server: {
+            baseDir: "./",
+            index: 'index.html',
+            port: 3000
+        }
+    });
+});
+
+gulp.task('scripts', () => {
+  gulp.src('jasmine/spec/invertedIndex-spec.js')
+    .pipe(browserify())
+    .pipe(rename('bundle.js'))
+    .pipe(gulp.dest('jasmine/testfiles'));
+});
+
+gulp.task('default', ['browserSync'], function() {
+    gulp.watch('public/css/*.css', browserSync.reload);
+    gulp.watch('public/js/*.js', browserSync.reload);
+    gulp.watch('*.html', browserSync.reload);
+    gulp.watch('src/*.js', browserSync.reload);
+});
