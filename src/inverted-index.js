2f18ee775ddf7dd128ea4af8be05ffa1e0ab46dd
andela-moseni <mercy.oseni@andela.com>
2017-03-18 09:14:20 +0100

    add comments for readability

--- src/inverted-index.js
+++ src/inverted-index.js
@@ -1,12 +1,12 @@
 /**
  * inverted index class
  * @class
- */
+**/
 class InvertedIndex {
   /**
   * class constructor
   * @constructor
-  */
+  **/
   constructor() {
     this.fileIndices = {};
   } 
@@ -15,30 +15,37 @@ class InvertedIndex {
    * @param {String} filename - Name of the indexed file
    * @param {Object} indices - Indices of the file
    * @return {object} Indexed file name and it's indices'
-   */
+  **/
   setIndex(filename, indices) {
     this.fileIndices[filename] = indices;
   }
+  /**
+   * Validate File
+   * It checks if a json file is a json array of json objects
+   * @param {Object} file is an array of json objects
+   * @return {Boolean} True if a json file is valid and False otherwise
+  **/
   validateFile(file) {
     if (typeof file !== 'object' || file.length === 0) {
       return false;
     }
-      for (let i = 0; i < file.length; i += 1) {
+      for (let i = 0; i < file.length; i++) {
         let item = file[i];
         if (!(item.hasOwnProperty('title') && item.hasOwnProperty('text'))) {
-          // console.log("Upload a valid json file");
           return false;
         }
       }
      return true;
   }
-  readFile(file) {
-    // this.file = file;
-    // JSON.parse(JSON.stringify(file));
-  }
+  /**
+   * Tokenize
+   * It splits sentence into an array of refined words
+   * @param {String} text - string of texts
+   * @return {Array} An array of refined splitted texts
+  **/
   tokenize(text) { 
-    const error = /[^\w\s]/g;
-    return text.replace(error, " ").toLowerCase().split(" ").sort().filter(item => Boolean(item));
+    const remove = /[^\w\s]/g;  
+    return text.replace(remove, " ").toLowerCase().split(" ").sort().filter(item => Boolean(item));
   }
   createIndex(fileName, fileContent) {
     const indices = {};
@@ -56,13 +63,17 @@ class InvertedIndex {
           }
         });
       });
-      indices.length = fileContent.length;
-      console.log(indices);
       this.setIndex(fileName, indices);
-        return 'Index created';
+      return 'Index created';
     }
-     return 'Index not created';
+    return 'Index not created';
   }
+  /**
+   * Get Index
+   * It gets the index of a specified filename
+   * @param {String} filename - Filename of the index to get
+   * @return {Object} An object of each word and their indices in a sorted way
+  **/
   getIndex(filename) {
     let newObj = {};
     let tokens = Object.keys(this.fileIndices[filename]).sort();
@@ -70,28 +81,5 @@ class InvertedIndex {
       newObj[token] = this.fileIndices[filename][token];
     });
     return newObj;
-  }
-  search(item, file) {
-    let searchResult = {};
-    searchResult = { index: {}, length: this.fileIndices[file].length };
-    item.forEach((text) => {
-      if (this.fileIndices[file].index.hasOwnProperty(text)) {
-        searchResult.index[text] = this.fileIndices[file].index[text];
-      }
-    });
-    return searchResult;
-  }
-  searchIndex(word, fileName) {
-    const searchResult = {};
-    word = word.split(' ').join(' ');
-    const newWord = this.tokenize(word);
-    if (fileName) {
-      Object.keys(this.fileIndices).forEach((files) => {
-        searchResult[files] = this.search(newWord, files);
-      });
-    } else {
-      searchResult[fileName] = this.search(newWord, fileName);
-    }
-    return searchResult;
   }
 }
